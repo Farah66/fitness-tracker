971a03355d1601b4ed3f8f0a57e3d190b0b2a71a
farah <farahnazqasimi46gmail.com>
2026-04-06 17:47:39 +0200

    Initial project setup

---
+++ demo/src/data/make_dataset.py
@@ -0,0 +1,227 @@
+from pathlib import Path
+from zipfile import Path
+import pandas as pd
+from glob import glob
+
+# --------------------------------------------------------------
+# Read single CSV file
+# %%
+# --------------------------------------------------------------
+# single_file_acc = pd.read_csv("../../data/raw/MetaMotion/A-bench-heavy_MetaWear_2019-01-14T14.22.49.165_C42732BE255C_Accelerometer_12.500Hz_1.4.4.csv")
+single_file_acc = pd.read_csv(
+    "../../data/raw/MetaMotion/A-bench-heavy_MetaWear_2019-01-14T14.22.49.165_C42732BE255C_Accelerometer_12.500Hz_1.4.4.csv"
+)
+single_file_gyr = pd.read_csv(
+    "../../data/raw/MetaMotion/A-bench-heavy_MetaWear_2019-01-14T14.22.49.165_C42732BE255C_Gyroscope_25.000Hz_1.4.4.csv"
+)
+
+print(single_file_acc.head())
+# --------------------------------------------------------------
+# List all data in data/raw/MetaMotion
+#%%
+# --------------------------------------------------------------
+file = glob("../../data/raw/MetaMotion/*.csv")
+
+len(file)
+
+# --------------------------------------------------------------
+# Extract features from filename
+# --------------------------------------------------------------
+#%%
+data_path = "../../data/raw/MetaMotion/"
+
+f = file[0]
+
+first_part = Path(f).stem.split("_")[0]
+parts = first_part.split("-")
+
+print(parts)
+
+participant = parts[0]
+label = parts[1]
+category = parts[2].rstrip("123")
+
+df = pd.read_csv(f)
+df["participant"] = participant
+df["label"] = label
+df["category"] = category
+
+df.head()
+
+
+df
+
+
+
+# --------------------------------------------------------------
+# Read all files
+# --------------------------------------------------------------
+acc_df = pd.DataFrame()
+gyr_df = pd.DataFrame()
+
+acc_set = 1
+gyr_set = 1
+
+for f in file:
+    first_part = Path(f).stem.split("_")[0]
+    parts = first_part.split("-")
+
+    participant = parts[0]
+    label = parts[1]
+    category = parts[2].rstrip("123")
+
+    df = pd.read_csv(f)
+
+    df["participant"] = participant
+    df["label"] = label
+    df["category"] = category
+
+    if "Accelerometer" in f:
+        df["set"] = acc_set
+        acc_set += 1
+        acc_df = pd.concat([acc_df, df], ignore_index=True)
+        
+    if "Gyroscope" in f:
+        df["set"] = gyr_set
+        gyr_set += 1
+        gyr_df = pd.concat([gyr_df, df], ignore_index=True)
+        
+        print("Reading:", f)
+df
+   
+# --------------------------------------------------------------
+# Working with datetimes
+# --------------------------------------------------------------
+acc_df.info()
+
+pd.to_datetime(df["epoch (ms)"], unit="ms")
+
+
+acc_df.index = pd.to_datetime(acc_df["epoch (ms)"], unit="ms")
+gyr_df.index = pd.to_datetime(gyr_df["epoch (ms)"], unit="ms")
+
+
+del acc_df["epoch (ms)"]
+del acc_df["time (01:00)"]
+del acc_df["elapsed (s)"]
+
+del gyr_df["epoch (ms)"] 
+del gyr_df["time (01:00)"]  
+del gyr_df["elapsed (s)"]
+
+acc_df.head() 
+# --------------------------------------------------------------
+# Turn into function
+# --------------------------------------------------------------
+
+file = glob("../../data/raw/MetaMotion/*.csv")
+
+def read_data_from_files(file):
+    acc_df = pd.DataFrame()
+    gyr_df = pd.DataFrame()
+
+    acc_set = 1
+    gyr_set = 1
+
+    for f in file:
+        first_part = Path(f).stem.split("_")[0]
+        parts = first_part.split("-")
+
+        participant = parts[0]
+        label = parts[1]
+        category = parts[2].rstrip("123")
+
+        df = pd.read_csv(f)
+
+        df["participant"] = participant
+        df["label"] = label
+        df["category"] = category
+
+        if "Accelerometer" in f:
+            df["set"] = acc_set
+            acc_set += 1
+            acc_df = pd.concat([acc_df, df], ignore_index=True)
+
+        elif "Gyroscope" in f:
+            df["set"] = gyr_set
+            gyr_set += 1
+            gyr_df = pd.concat([gyr_df, df], ignore_index=True)
+
+    acc_df.index = pd.to_datetime(acc_df["epoch (ms)"], unit="ms")
+    gyr_df.index = pd.to_datetime(gyr_df["epoch (ms)"], unit="ms")
+
+    del acc_df["epoch (ms)"]
+    del acc_df["time (01:00)"]
+    del acc_df["elapsed (s)"]
+
+    del gyr_df["epoch (ms)"]
+    del gyr_df["time (01:00)"]
+    del gyr_df["elapsed (s)"]
+
+    return acc_df, gyr_df
+
+acc_df, gyr_df = read_data_from_files(file)   
+
+# --------------------------------------------------------------
+# Merging datasets
+# --------------------------------------------------------------
+
+data_merged = pd.concat([acc_df.iloc[:, :3], gyr_df], axis=1)
+data_merged.dropna()
+
+# Renaming the columns
+data_merged.columns = [
+    "acc_x",
+    "acc_y",
+    "acc_z",
+    "gyr_x",
+    "gyr_y",
+    "gyr_z",
+    "participant",
+    "label",
+    "category",
+    "set",
+]
+# --------------------------------------------------------------
+# Resample data (frequency conversion)
+# --------------------------------------------------------------
+
+# Accelerometer:    12.500HZ
+# Gyroscope:        25.000Hz
+
+sampling = {
+    "acc_x": "mean",
+    "acc_y": "mean",
+    "acc_z": "mean",
+    "gyr_x": "mean",
+    "gyr_y": "mean",
+    "gyr_z": "mean",
+    "participant": "last",
+    "label": "last",
+    "category": "last",
+    "set": "last",
+}
+
+data_merged.iloc[:100].resample("200ms").agg(sampling)
+ 
+# split by day 
+days = [g for n, g in data_merged.groupby(pd.Grouper(freq="D"))]
+
+data_resampled = pd.concat(
+    [df.resample("200ms").agg(sampling).dropna() for df in days]
+)
+
+data_resampled["set"] = data_resampled["set"].astype(int) 
+data_resampled.info() 
+
+# --------------------------------------------------------------
+# Export dataset
+# --------------------------------------------------------------
+data_resampled.to_pickle("../../data/interim/01_data_processed.pkl")
+
+# print("acc labels:", sorted(acc_df["label"].dropna().unique()))
+# print("gyr labels:", sorted(gyr_df["label"].dropna().unique()))
+# print("merged labels:", sorted(data_merged["label"].dropna().unique()))
+# print("resampled labels:", sorted(data_resampled["label"].dropna().unique()))
+
+
