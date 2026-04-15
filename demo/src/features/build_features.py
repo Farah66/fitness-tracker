b851efa020052595ddbaf22bffddbf0d46ab9b8b
farah <farahnazqasimi46gmail.com>
2026-04-15 16:04:12 +0200

    Temporal abstraction, Frequency features, visualize result , Dealing with overlapping windows

--- demo/src/features/build_features.py
+++ demo/src/features/build_features.py
@@ -221,15 +221,78 @@ df_freq = pd.concat(df_freq.list).set_index("epoch (ms)", drop=True)
 # Dealing with overlapping windows
 # --------------------------------------------------------------
 df_freq = df_freq.dropna()
-
-df_freq.iloc[::2]
+df_freq= df_freq.iloc[::2]
 
 # --------------------------------------------------------------
 # Clustering
 # --------------------------------------------------------------
+df_cluster = df_freq.copy()
 
 
+cluster_columns = ["acc_x", "acc_y", "acc_z"]
+k_values = range(2, 10)
+inertias = []
+
+for k in k_values:
+    subset = df_cluster[cluster_columns].dropna()
+    kmeans = KMeans(n_clusters=k, n_init=20, random_state=0)
+    cluster_labels = kmeans.fit_predict(subset)
+    inertias.append(kmeans.inertia_)
+
+plt.figure(figsize=(10, 10))
+plt.plot(k_values, inertias)    
+plt.xlabel("k")
+plt.ylabel("sum of squared distances")
+plt.show()
+
+kmeans = KMeans(n_clusters=3, n_init=20, random_state=0)
+subset = df_cluster[cluster_columns].dropna()
+df_cluster["cluster"] = kmeans.fit_predict(subset)
+
+# Plot clusters
+fig = plt.figure(figsize=(15, 15))
+ax = fig.add_subplot(projection='3d')
+
+for c in df_cluster["cluster"].unique():
+    subset = df_cluster[df_cluster["cluster"] == c]
+    
+    ax.scatter(
+        subset["acc_x"],
+        subset["acc_y"],
+        subset["acc_z"],
+        label=c
+    )
+
+ax.set_xlabel("x-axis")
+ax.set_ylabel("y-axis")
+ax.set_zlabel("z-axis")
+
+plt.legend()
+plt.show()
+
+# plot accelerometer data to compare with clusters
+fig = plt.figure(figsize=(15, 15))
+ax = fig.add_subplot(projection='3d')
+
+for I in df_cluster["label"].unique():
+    subset = df_cluster[df_cluster["label"] == I]
+    
+    ax.scatter(
+        subset["acc_x"],
+        subset["acc_y"],
+        subset["acc_z"],
+        label=I
+    )
+
+ax.set_xlabel("x-axis")
+ax.set_ylabel("y-axis")
+ax.set_zlabel("z-axis")
+
+plt.legend()
+plt.show()
 # --------------------------------------------------------------
 # Export dataset
+df_cluster.to_pickle("../../data/interim/03_data_features.pkl")
 # --------------------------------------------------------------
 # %%
+ 
